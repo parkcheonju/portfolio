ce154e22227d334b7c482772a863e4282b172a94
parkcheonju <cjswne@gmail.com>
2023-04-08 21:47:43 +0900

    section3 update commit

--- src/components/Sections/Section3.js
+++ src/components/Sections/Section3.js
@@ -7,17 +7,31 @@ export default function Section3() {
       <div className="Section3_inner">
         <div className="Section3_container">
           <div className="Section3_top">
-            <div className='Section3_top-title'>
+            <div className="Section3_top-title">
               <h2>Latest Works</h2>
             </div>
-            <div className='Section3_top-list'>
+            <div className="Section3_top-list">
               <span>All Projects</span>
               <img src="/images/dummyicon.png" alt="linkicon" />
             </div>
           </div>
           <div className="Section3_bottom">
-            <div>
-              
+            <div className="Section3_bottom-container">
+              <div className="Section3_bottom-card">
+                <div className="Section3_bottom-item">img</div>
+                <div className="Section3_item-title">title</div>
+                <div className="Section3_item-description"><h4>description</h4></div>
+              </div>
+              <div className="Section3_bottom-card">
+                <div className="Section3_bottom-item">img</div>
+                <div className="Section3_item-title">title</div>
+                <div className="Section3_item-description"><h4>description</h4></div>
+              </div>
+              <div className="Section3_bottom-card">
+                <div className="Section3_bottom-item">img</div>
+                <div className="Section3_item-title">title</div>
+                <div className="Section3_item-description"><h4>description</h4></div>
+              </div>
             </div>
           </div>
         </div>
